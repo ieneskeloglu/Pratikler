0770c38c14b25c955a1314b32e6331bd4fbb8da3
İhsan Enes <ieneskeloglu2@outlook.com>
2022-10-15 21:14:55 +0300

    bir

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("A, World!");
+        Console.WriteLine("merhaba");
+        Console.WriteLine("merhaba");
+
+        
+
+    }
+}
